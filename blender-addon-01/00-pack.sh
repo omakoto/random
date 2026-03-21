99714be14fc37535e15b2e256c6777016dcbe126
Makoto Onuki <makoto.onuki@gmail.com>
2026-03-21 13:33:04 -0700

    Fix zip structure for Blender compatibility

--- blender-addon-01/00-pack.sh
+++ blender-addon-01/00-pack.sh
@@ -9,8 +9,12 @@ ZIP_FILE="dist/${ADDON_NAME}.zip"
 # Remove existing zip if it exists
 rm -f "$ZIP_FILE"
 
-# Zip the contents of the src directory
-# We change directory into src so that __init__.py is at the root of the zip
-(cd src && zip -r "../$ZIP_FILE" .)
+# Zip the contents of the src directory into a folder named after the addon
+# Blender expects the __init__.py to be inside a subdirectory in the zip
+rm -rf "dist/${ADDON_NAME}"
+mkdir -p "dist/${ADDON_NAME}"
+cp -r src/* "dist/${ADDON_NAME}/"
+(cd dist && zip -r "../$ZIP_FILE" "${ADDON_NAME}")
+rm -rf "dist/${ADDON_NAME}"
 
 echo "Package created: $ZIP_FILE"
